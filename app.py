79789a3b64f7dceba5f9c650ad3cd00f37028101
OMAR730283 <ofnations1234@gmail.com>
2026-08-16 03:08:55 +0200

    database set up

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 import flask
+from flask_sqlalchemy import SQLAlchemy
+
+#sqllite
 
 app = flask.Flask(__name__)
+
+db = SQLAlchemy()
+
+app.config["SQLALCHEMY_DATABASE_URI"]  = "sqlite:///database.db"
+
+db.init_app(app)
+
+class Task(db.Model):
+   __tablename__ = "Tasks"
+
+   id = db.Column(db.Integer, primary_key = True)
+   title = db.Column(db.String)
+   description = db.Column(db.String)
+   completed = db.Column(db.Boolean)
+
+
+with app.app_context():
+   db.create_all()
 
 @app.route('/hello/<name>')
 def hello(name):
