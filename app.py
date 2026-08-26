a847716fe323bbe9cc39de2378b41d8b8f0c70f4
OMAR730283 <ofnations1234@gmail.com>
2026-08-27 01:09:27 +0200

    crud functionalities

--- app.py
+++ app.py
@@ -29,17 +29,10 @@ with app.app_context():
    # )
    # db.session.add(task)
    # db.session.commit()
-
-   # tasks = Task.query.all()
-   # for task in tasks:
-   #  print(task.description)
-
-  
-@app.route('/hello/<name>')
-def hello(name):
-  return "hillohoi " + name
-
-
+   # task = Task.query.get(1)
+   # task.title = "omar faruk"
+   # print(task.title)
+   # db.session.commit()
 
 @app.route('/', methods=["GET", "POST"])
 def home():
@@ -61,5 +54,31 @@ def home():
        
     return flask.render_template('index.html',  tasks = Task.query.all() )
 
+
+@app.route('/update/<int:id>', methods=["GET", "POST"])
+
+def update(id):
+  task = Task.query.get(id)
+  if not task:
+     return "id not found"
+  if flask.request.method == "POST":
+        task.title = flask.request.form["title"]
+        task.description = flask.request.form["description"]
+        task.completed = flask.request.form.get("completed") == "True"
+
+        db.session.commit()
+
+  return flask.render_template("update.html", task=task)
+
+@app.route('/delete/<int:id>', methods = ["POST"])
+
+def delete(id):
+   task = Task.query.get(id)
+   if not task:
+      return "id not found"
+   db.session.delete(task)   
+   db.session.commit()
+   return flask.redirect(flask.url_for("home"))
+ 
 app.run(debug=True)
 
